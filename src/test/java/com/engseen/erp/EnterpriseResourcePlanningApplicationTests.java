b71aa7b234be3c187445fe2a311b6e2b47a90040
HowardYaw <yjhhoward@gmail.com>
2021-12-17 22:26:08 +0800

    spring initialzr setup

---
+++ src/test/java/com/engseen/erp/EnterpriseResourcePlanningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engseen.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseResourcePlanningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
