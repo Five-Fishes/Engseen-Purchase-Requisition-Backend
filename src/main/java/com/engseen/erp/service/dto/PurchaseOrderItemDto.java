e9b05e08ce2caad25576dbf82c187583c69c8181
HowardYaw <yjhhoward@gmail.com>
2022-01-02 19:20:13 +0800

    add annotation

--- src/main/java/com/engseen/erp/service/dto/PurchaseOrderItemDto.java
+++ src/main/java/com/engseen/erp/service/dto/PurchaseOrderItemDto.java
@@ -3,11 +3,13 @@ package com.engseen.erp.service.dto;
 import java.io.Serializable;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * A DTO for the {@link com.thirdcc.webapp.entity.PurchaseOrderItem} entity.
  */
 @Data
+@EqualsAndHashCode(callSuper=false)
 public class PurchaseOrderItemDto extends PurchaseRequestItemDto implements Serializable {
 
     private Long id;
--- src/main/java/com/engseen/erp/service/dto/PurchaseRequestApprovalItemDto.java
+++ src/main/java/com/engseen/erp/service/dto/PurchaseRequestApprovalItemDto.java
@@ -5,11 +5,13 @@ import java.io.Serializable;
 import com.engseen.erp.constant.enumeration.PurchaseRequisitionApprovalItemStatus;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * A DTO for the {@link com.thirdcc.webapp.entity.PurchaseRequestApprovalItem} entity.
  */
 @Data
+@EqualsAndHashCode(callSuper=false)
 public class PurchaseRequestApprovalItemDto extends PurchaseRequestItemDto implements Serializable {
 
     private Long requestApprovalId;
