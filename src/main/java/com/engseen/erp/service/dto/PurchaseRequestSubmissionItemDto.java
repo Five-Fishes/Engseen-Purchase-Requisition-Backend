67c9b2f1bf76ffe781cb58a968a805b12eb3abc3
HowardYaw <yjhhoward@gmail.com>
2022-01-04 20:21:25 +0800

    add lombok annotation

--- src/main/java/com/engseen/erp/service/dto/PurchaseRequestSubmissionItemDto.java
+++ src/main/java/com/engseen/erp/service/dto/PurchaseRequestSubmissionItemDto.java
@@ -3,11 +3,13 @@ package com.engseen.erp.service.dto;
 import java.io.Serializable;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * A DTO for the {@link com.engseen.erp.entity.PurchaseRequestSubmissionItem} entity.
  */
 @Data
+@EqualsAndHashCode(callSuper=false)
 public class PurchaseRequestSubmissionItemDto extends PurchaseRequestItemDto implements Serializable {
 
     private Long id;
