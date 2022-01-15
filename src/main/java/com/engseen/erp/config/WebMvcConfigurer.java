cb85945a64b4d6abbb33a77b2190d258e7813acf
HowardYaw <yjhhoward@gmail.com>
2022-01-15 15:59:50 +0800

    enable cors and sample data sql

---
+++ src/main/java/com/engseen/erp/config/WebMvcConfigurer.java
@@ -0,0 +1,22 @@
+package com.engseen.erp.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class WebMvcConfigurer {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfig = new CorsConfiguration().applyPermitDefaultValues();
+        source.registerCorsConfiguration("/api/**", corsConfig);
+        return new CorsFilter(source);
+    }
+    
+}
