754ff3648b1bbd95b4ecbec9eb4724a3e9e6f54e
LUXIANZE <luxianze98@gmail.com>
2022-02-18 15:22:06 +0800

    Implement User Login

--- src/main/java/com/engseen/erp/service/impl/UserServiceImpl.java
+++ src/main/java/com/engseen/erp/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.engseen.erp.service.impl;
 import com.engseen.erp.domain.UserMaster;
 import com.engseen.erp.repository.UserMasterRepository;
 import com.engseen.erp.service.UserService;
+import com.engseen.erp.service.dto.LoginDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -19,6 +20,24 @@ public class UserServiceImpl implements UserService {
     @Override
     public String getUserAuthority(String userId) {
         Optional<UserMaster> userMasterOptional = userMasterRepository.findOneByUserID(userId);
-        return userMasterOptional.isPresent() ? userMasterOptional.get().getRole() : "";
+        return userMasterOptional.isPresent() ? userMasterOptional.get().getUserGroup() : "";
+    }
+
+    @Override
+    public String login(LoginDTO loginDTO) throws Exception {
+        Optional<UserMaster> userMasterOptional = userMasterRepository.findOneByUserID(loginDTO.getUserId());
+        if (userMasterOptional.isPresent()) {
+            UserMaster userMaster = userMasterOptional.get();
+            boolean isPasswordMatch = userMaster.getPassword().equals(loginDTO.getPassword());
+
+            if (isPasswordMatch) {
+                boolean isAdmin = userMaster.getUserGroup().equalsIgnoreCase("Admin");
+                return isAdmin ? "Admin" : "Normal";
+            } else {
+                throw new Exception("Invalid Password");
+            }
+        } else {
+            throw new Exception("User ID Not Found");
+        }
     }
 }
