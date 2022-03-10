ffa4cacb7dd034bf81dd5467042ad4ce79df49c8
LUXIANZE <luxianze98@gmail.com>
2022-03-11 00:54:02 +0800

    PO Template CSS fix - Remove the use of column-count as iText does not support

--- src/main/java/com/engseen/erp/service/PurchaseOrderService.java
+++ src/main/java/com/engseen/erp/service/PurchaseOrderService.java
@@ -14,22 +14,22 @@ import org.springframework.data.domain.Pageable;
  */
 public interface PurchaseOrderService {
 
-	/**
+    /**
      * Get all the PurchaseOrderRequestApproval.
      *
      * @param pageable the pagination information.
      * @return the list of entities group by Purchase Request Approval.
      */
-	List<PurchaseOrderRequestApprovalDto> findAllGroupByPurchaseRequestApproval(Pageable pageable);
+    List<PurchaseOrderRequestApprovalDto> findAllGroupByPurchaseRequestApproval(Pageable pageable);
 
     /**
      * Get all the PurchaseOrder by Purchase Request Approval Id.
      *
      * @param purchaseRequestApprovalId purchase requisition approval id
-     * @param pageable the pagination information.
+     * @param pageable                  the pagination information.
      * @return the list of entities.
      */
-	List<PurchaseOrderDto> findAllByPurchaseRequestApprovalId(Long purchaseRequestApprovalId, Pageable pageable);
+    List<PurchaseOrderDto> findAllByPurchaseRequestApprovalId(Long purchaseRequestApprovalId, Pageable pageable);
 
     /**
      * Issue PO based on vendor
@@ -39,31 +39,39 @@ public interface PurchaseOrderService {
      * @return the list of PO issue/created
      * @throws Exception exception while issuing PO
      */
-	List<PurchaseOrderDto> issuePO(Long purchaseRequestApprovalId) throws Exception;
+    List<PurchaseOrderDto> issuePO(Long purchaseRequestApprovalId) throws Exception;
 
     /**
      * Email PO to vendor
-     * 
+     *
      * @param purchaseOrderId id of PO to email
      * @return status of email sent
      * @throws IOException exception while mailing PO
      */
-	Boolean emailPO(Long purchaseOrderId) throws Exception;
+    Boolean emailPO(Long purchaseOrderId) throws Exception;
 
     /**
      * Generate PO as PDF file to download
-     *  
+     *
      * @param purchaseOrderId exception while generating PO pdf base 64 string
      * @return Base64 encoded String of PO PDF file
      */
-	String downloadPOPdfBase64String(Long purchaseOrderId) throws IOException;
+    String downloadPOPdfBase64String(Long purchaseOrderId) throws IOException;
 
-	/**
-	 * Generate PO as PDF file to download
-	 *
-	 * @param purchaseOrderId Id of purchase order
-	 * @return generated PO pdf file
-	 * @throws IOException Exception generating PO pdf file
-	 */
-	File downloadPOFile(Long purchaseOrderId) throws IOException;
+    /**
+     * Generate PO as PDF file to download
+     *
+     * @param purchaseOrderId Id of purchase order
+     * @return generated PO pdf file
+     * @throws IOException Exception generating PO pdf file
+     */
+    File downloadPOFile(Long purchaseOrderId) throws IOException;
+
+    /**
+     * Generate HTML String to be used for PDF generation
+     *
+     * @param purchaseOrderId ID of purchase order to generate an HTML String
+     * @return HTML String
+     */
+    String getPOHtml(Long purchaseOrderId);
 }
